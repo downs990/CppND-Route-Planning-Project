0befeea0cf964f4846905819e40f7ab6b8cf3fd6
downs990 <downscourtney17@gmail.com>
2021-10-10 01:32:45 +0000

    completed the first TODO item

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,11 @@
 #include "route_model.h"
 #include "render.h"
 #include "route_planner.h"
+using std::cout;
+using std::cin;
 
 using namespace std::experimental;
-
-// Plaese save this time. // first // git test
+ 
 static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
 {   
     std::ifstream is{path, std::ios::binary | std::ios::ate};
@@ -54,15 +55,40 @@ int main(int argc, const char **argv)
     }
 
 
+
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    cout << "Enter float for Start X: ";
+    cin >> start_x;
+
+    cout << "Enter float for Start Y: ";
+    cin >> start_y; 
+
+    cout << "Enter float for End X: ";
+    cin >> end_x;
+
+    cout << "Enter float for End Y: ";
+    cin >> end_y;
+
+    // cout << "Values: " << start_x << " " << start_y << " " << end_x << " " << end_y;
+ 
+
+    // NOTE: How to build the project. 
+    // 1. Navigate to build dir
+    // 2. cmake ..
+    // 3. make 
+    // 4. ./OSM_A_star_search
+
+
+
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
